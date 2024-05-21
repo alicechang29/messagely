0f5617ba4c7adb46ef0695abbfaa232e217eda46
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-20 17:25:56 -0700

    get message by id and post message routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,6 @@
 import express from "express";
+import Message from "../models/message.js";
+import User from "..models/user.js";
 
 const router = new express.Router();
 
@@ -14,6 +16,13 @@ const router = new express.Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", async function () {
+  const msgId = req.params.id;
+
+  const message = await Message.get(msgId);
+
+  return { message };
+});
 
 
 /** POST / - post message.
@@ -22,6 +31,16 @@ const router = new express.Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+
+router.post("/", async function () {
+  const fromUsername = res.locals.user;
+  const toUsername = req.body.to_username;
+  const body = req.body.body;
+
+  const message = await Message.create(fromUsername, toUsername, body);
+
+  return { message };
+});
 
 
 /** POST/:id/read - mark message as read:
