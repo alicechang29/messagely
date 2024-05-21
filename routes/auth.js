92fc9019f2b9065af630dab8ddc0987b928b2349
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-20 17:02:22 -0700

    auth routes - login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,33 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+import { UnauthorizedError, BadRequestError } from "../expressError.js";
+import { SECRET_KEY } from "../config.js";
+import User from "../models/user.js";
 
 const router = new express.Router();
 
 /** POST /login: {username, password} => {token} */
 
+router.post("/login", async function (req, res, next) {
+  if (
+    req.body === undefined ||
+    req.body.username === undefined ||
+    req.body.password === undefined
+  ) {
+    throw new BadRequestError();
+  }
+
+  const { username, password } = req.body;
+
+  if (await User.authenticate(username, password) === true) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  };
+
+  throw new UnauthorizedError();
+
+});
 
 /** POST /register: registers, logs in, and returns token.
  *
